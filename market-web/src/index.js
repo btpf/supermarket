8a9b6c3544b87b3873c5768c9fb2e21bf26f26a7
btpf <bretpapkoff@Gmail.com>
2020-09-03 22:06:52 -0400

    Added react-router

--- market-web/src/index.js
+++ market-web/src/index.js
@@ -1,7 +1,31 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+
+function World() {
+  return <h1>Hello World!</h1>
+}
+
+function Hello() {
+  return (<>
+    <h1>Hello</h1>
+    <NavLink to="/world">
+      hello who?
+</NavLink>
+  </>);
+}
 
 ReactDOM.render(
-    <h1>Hello, worlds!</h1>,
-    document.getElementById('root')
-  );
+  <BrowserRouter>
+    <Switch>
+    <Route exact path="/">
+          <Hello />
+        </Route>
+      <Route path="/world">
+        <World />
+      </Route>
+
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
--- market-web/webpack.config.js
+++ market-web/webpack.config.js
@@ -8,5 +8,12 @@ module.exports = {
     rules: [
       { test: /\.js$/, use: 'babel-loader' }
     ]
-  }
+  },
+  devServer: {
+    historyApiFallback: true, // https://ui.dev/react-router-cannot-get-url-refresh/
+    // https://webpack.js.org/configuration/dev-server/#devserverhistoryapifallback
+    // Needed because react-router checks the URL and navigates. But is loaded from index.html.
+    // Since webpack-dev-server is serving all files in directory, it cannot find the file for things after the '/' unless its a file
+    // Redirecting first to the index.html fixes this.
+  },
 };
